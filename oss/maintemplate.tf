cca34899924085b80965fb638449899f01457423
unknown <srekapalli@sysgain.com>
2017-09-21 16:41:00 +0530

    updated

--- oss/maintemplate.tf
+++ oss/maintemplate.tf
@@ -32,3 +32,34 @@ address_prefix = "${var.Subnet3}"
 resource_group_name = "${azurerm_resource_group.ResourceGroup.name}"
 virtual_network_name = "${azurerm_virtual_network.Vnet.name}"
 }
+resource "azurerm_network_security_group" "Nsg" {
+  name                = "nsg"
+  location            = "${var.Location}"
+  resource_group_name = "${azurerm_resource_group.ResourceGroup.name}"
+}
+resource "azurerm_network_security_rule" "SSH" {
+  name                        = "ssh"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "${var.Subnet1}"
+  destination_address_prefix  = "${var.Subnet1}"
+  resource_group_name         = "${azurerm_resource_group.ResourceGroup.name}"
+  network_security_group_name = "${azurerm_network_security_group.Nsg.name}"
+}
+resource "random_id" "dns" {
+  keepers = {
+    dnsid = "${var.DynamicDNS}"
+  }
+  byte_length = 8
+}
+resource "azurerm_public_ip" "DyanamicIP" {
+  name                         = "DynamicIP2"
+  location                     = "${var.Location}"
+  resource_group_name          = "${azurerm_resource_group.ResourceGroup.name}"
+  public_ip_address_allocation = "${var.DynamicIP}"
+  domain_name_label = "saf${random_id.dns.hex}"
+}
