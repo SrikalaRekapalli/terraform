659882d3f672001cc16be9172283fa6b7bfb3994
unknown <srekapalli@sysgain.com>
2017-09-21 16:29:00 +0530

    updated

--- nested/maintemplate.tf
+++ nested/maintemplate.tf
@@ -1,6 +1,12 @@
+provider "azurerm" {
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+}
 resource "azurerm_resource_group" "ResourceGroup" {
-    name = "${var.ResourceGroup}"
-    location = "${var.Location}"
+name = "${var.ResourceGroup}"
+location = "${var.Location}"
 }
 resource "azurerm_virtual_network" "Vnet" {
 name = "Vnet"
