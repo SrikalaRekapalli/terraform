bfa9d318273b28037576abaa75a683505775e01f
unknown <srekapalli@sysgain.com>
2017-09-22 19:07:15 +0530

    updated

--- oss/maintemplate.tf
+++ oss/maintemplate.tf
@@ -102,7 +102,7 @@ resource "random_id" "uniq" {
   keepers = {
     unique_id = "${var.uniqueid}"
   }
-  byte_length = 8
+  byte_length = 2
 }
 resource "azurerm_storage_account" "Storage" {
 name                = "strg${random_id.uniq.hex}"
