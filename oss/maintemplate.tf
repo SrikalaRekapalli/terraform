1f432649e1a77bc6c215f660bce251664e2b4e3d
unknown <srekapalli@sysgain.com>
2017-09-22 17:07:45 +0530

    updated

--- oss/maintemplate.tf
+++ oss/maintemplate.tf
@@ -1,9 +1,3 @@
-provider "azurerm" {
-subscription_id = "${var.subscription_id}"
-client_id       = "${var.client_id}"
-client_secret   = "${var.client_secret}"
-tenant_id       = "${var.tenant_id}"
-}
 resource "azurerm_resource_group" "ResourceGroup" {
 name = "${var.ResourceGroup}"
 location = "${var.Location}"
@@ -17,18 +11,6 @@ resource_group_name = "${azurerm_resource_group.ResourceGroup.name}"
 resource "azurerm_subnet" "subnet1" {
 name = "Subnet1"
 address_prefix = "${var.Subnet1}"
-resource_group_name = "${azurerm_resource_group.ResourceGroup.name}"
-virtual_network_name = "${azurerm_virtual_network.Vnet.name}"
-}
-resource "azurerm_subnet" "subnet2" {
-name = "Subnet2"
-address_prefix = "${var.Subnet2}"
-resource_group_name = "${azurerm_resource_group.ResourceGroup.name}"
-virtual_network_name = "${azurerm_virtual_network.Vnet.name}"
-}
-resource "azurerm_subnet" "subnet3" {
-name = "Subnet3"
-address_prefix = "${var.Subnet3}"
 resource_group_name = "${azurerm_resource_group.ResourceGroup.name}"
 virtual_network_name = "${azurerm_virtual_network.Vnet.name}"
 }
@@ -52,7 +34,7 @@ resource "azurerm_network_security_rule" "SSH" {
 }
 resource "random_id" "uniq" {
   keepers = {
-    storageid = "${var.storageAccid}"
+    uniqueid = "${var.uniqueid}"
   }
   byte_length = 8
 }
